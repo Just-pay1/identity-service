d746b5490c93c040138c14ef653c6e14b00c00c0
mahmoud ali <hodabelasy@gmail.com>
2024-11-27 21:49:11 +0200

    solve conflict

--- src/controller/AuthApiController.ts
+++ src/controller/AuthApiController.ts
@@ -1,20 +1,30 @@
-import { Request, Response } from "express";
-import User from "../models/userModel";
-import { where } from "sequelize";
-require("dotenv").config();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+import { Request, Response } from 'express';
+import User from '../models/userModel';
+import { where } from 'sequelize';
+
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+const ACCESS_TOKEN_LIFETIME = process.env.ACCESS_TOKEN_LIFETIME;
+const REFRESH_TOKEN_LIFETIME = process.env.REFRESH_TOKEN_LIFETIME;
 const generateOTP = require('../util/OTP').generateOTP;
 const setExpirationTime = require('../util/OTP').setExpirationTime;
 const sendOTPEmail = require('../util/OTP').sendOTPEmail;
 
-const JWT_SECRET = process.env.JWT_SECRET;
+
 
 exports.register = async (req: Request, res: Response) => {
     try {
         const otp = generateOTP();
         const otpExpiredAt = setExpirationTime();
         const { name, email, password, phone } = req.body;
+        const unique = await isEmailUnique(email)
+        if (!unique) {
+            res.status(409).json({ error: 'This email address is already registered. Please use a different email or log in.' });
+        }
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = new User({ name, email, phone, password: hashedPassword, otp, otp_expired_at: otpExpiredAt });
@@ -24,8 +34,9 @@ exports.register = async (req: Request, res: Response) => {
         await user.save();
         // res.send('here')
 
-        const token = generateToken(user);
-        res.status(200).json({ user, token });
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+        res.status(200).json({ user, accessToken, refreshToken });  
     } catch (error: any) {
         console.error("Registration error:", error);
         res.status(500).json({ error: "Registration failed", details: error.message });
@@ -35,24 +46,51 @@ exports.register = async (req: Request, res: Response) => {
 exports.login = async (req: Request, res: Response) => {
     try {
         const { email, password } = req.body;
-        const user = await User.findOne({ where: { email } });
+        const user = await User.findOne({where :{ email}});
         if (!user) {
-            return res.status(401).json({ error: "Authentication failed" });
+            return res.status(401).json({ error: 'Authentication failed' });
         }
-
+      
         // res.send(await bcrypt.compare(password, user.password))
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
-            return res.status(401).json({ error: "Authentication failed" });
+            return res.status(401).json({ error: 'Authentication failed' });
         }
 
-        const token = generateToken(user);
-        res.status(200).json({ user, token });
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+        res.status(200).json({ user, accessToken, refreshToken });
     } catch (error) {
-        res.status(500).json({ error: "Login failed" });
+        res.status(500).json({ error: 'Login failed' });
     }
-};
+  
+}
 
-const generateToken = (user: any) => {
-    return jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "30m" });
-};
+exports.refreshToken = async (req : Request, res: Response) => {
+    const { refreshToken} = req.body;
+    if (!refreshToken) return res.status(401).json({ error: 'Refresh token required' });
+
+    try{
+        const decoded = jwt.verify(refreshToken, JWT_REFRESH_SECRET) as {id : string};
+        const user = await User.findByPk(decoded.id)
+        if (!user) return res.status(401).json({ error: 'Invalid refresh token' });
+        const newAccessToken = generateAccessToken(user);
+        res.status(200).json({ accessToken: newAccessToken });
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid or expired refresh token' });
+    }
+}
+
+const generateAccessToken = (user : any) => {
+    return jwt.sign({ id: user.id }, JWT_ACCESS_SECRET, { expiresIn: ACCESS_TOKEN_LIFETIME });
+}
+const generateRefreshToken = (user : any) => {
+    return jwt.sign({ id: user.id }, JWT_REFRESH_SECRET, { expiresIn: REFRESH_TOKEN_LIFETIME });
+}
+
+const isEmailUnique = async(email : string) => {
+    const user = await User.findOne({ 
+        where: { email: email}
+    });
+    return !user;
+}
